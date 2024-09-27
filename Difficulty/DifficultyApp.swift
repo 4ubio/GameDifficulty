16f7d7d6174664c2261832c80e0f3e635b47953b
Sebastián Rubio Quiroz <sebastian.rubio@iest.edu.mx>
2024-09-27 17:05:16 -0600

    Initial Commit

---
+++ Difficulty/DifficultyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DifficultyApp.swift
+//  Difficulty
+//
+//  Created by Sebastián Rubio on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct DifficultyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DifficultyTests/DifficultyTests.swift
@@ -0,0 +1,17 @@
+//
+//  DifficultyTests.swift
+//  DifficultyTests
+//
+//  Created by Sebastián Rubio on 27/09/24.
+//
+
+import Testing
+@testable import Difficulty
+
+struct DifficultyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
